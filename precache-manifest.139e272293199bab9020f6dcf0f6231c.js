57bf66bc58d9420b69e457d9e95125ccddced4e6
Fredi Wilkens <frediwilkens@gmail.com>
2022-03-24 19:34:59 -0300

    Updates

---
+++ precache-manifest.139e272293199bab9020f6dcf0f6231c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a01fbf2df05bee9fdc9485a4d1f3b3",
+    "url": "/fw-wallet/index.html"
+  },
+  {
+    "revision": "f87d57a635f3ea2f1ef4",
+    "url": "/fw-wallet/static/css/main.e1c67efd.chunk.css"
+  },
+  {
+    "revision": "6eb42f00fdbdcba80232",
+    "url": "/fw-wallet/static/js/2.097a090c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fw-wallet/static/js/2.097a090c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87d57a635f3ea2f1ef4",
+    "url": "/fw-wallet/static/js/main.6638dd14.chunk.js"
+  },
+  {
+    "revision": "048193dc327b8892c544",
+    "url": "/fw-wallet/static/js/runtime-main.687f91ba.js"
+  }
+]);
